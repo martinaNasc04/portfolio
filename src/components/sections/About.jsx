cdd17cd03a823f54d5ef8446aa626e696ed14c31
Martina <martinanascimento95@gmail.com>
2026-03-02 21:55:04 -0300

    Update package, add new projects to Projects section, format code in About section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,59 +1,86 @@
-import { RevealOnScroll } from "../RevealOnScroll"
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
-    const frontendSkills = ["React", "TailwindCSS"]
-    const backendSkills = ["Python", "Django"]
+    const frontendSkills = ["Next.js", "React", "TailwindCSS"];
+    const backendSkills = ["Python", "Django"];
 
-    return <section id="about" className="min-h-screen flex items-center justify-center py-20">
-        <RevealOnScroll>
-            <div className="max-w-3xl mx-auto px-4">
-                <h2 className="text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-400
-            bg-clip-text text-transparent text-center">Sobre mim</h2>
-                {/* Habilidades */}
-                <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                    <p className="text-gray-300 mb-2">
-                        Desenvolvedora web com foco em front-end e um pouco de experiência em back-end.
-                    </p>
+    return (
+        <section
+            id="about"
+            className="min-h-screen flex items-center justify-center py-20"
+        >
+            <RevealOnScroll>
+                <div className="max-w-3xl mx-auto px-4">
+                    <h2
+                        className="text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-400
+            bg-clip-text text-transparent text-center"
+                    >
+                        Sobre mim
+                    </h2>
+                    {/* Habilidades */}
+                    <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                        <p className="text-gray-300 mb-2">
+                            Desenvolvedora web com foco em front-end e um pouco
+                            de experiência em back-end.
+                        </p>
 
-                    <div className="grid grid-cols-2 md:grid-cols-2 gap-6">
-                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                            <h3 className="text-lg font-bold mb-4">Frontend</h3>
-                            <div className="flex flex-wrap gap-2">
-                                {frontendSkills.map((tech, key) => (
-                                    <span key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{tech}</span>
-                                ))}
+                        <div className="grid grid-cols-2 md:grid-cols-2 gap-6">
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className="text-lg font-bold mb-4">
+                                    Frontend
+                                </h3>
+                                <div className="flex flex-wrap  gap-2">
+                                    {frontendSkills.map((tech, key) => (
+                                        <span
+                                            key={key}
+                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                                        >
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                            <h3 className="text-lg font-bold mb-4">Backend</h3>
-                            <div className="flex flex-wrap gap-2">
-                                {backendSkills.map((tech, key) => (
-                                    <span key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{tech}</span>
-                                ))}
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className="text-lg font-bold mb-4">
+                                    Backend
+                                </h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {backendSkills.map((tech, key) => (
+                                        <span
+                                            key={key}
+                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                                        >
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                {/* Educação */}
-                <div className="grid grid-cols-2 md:grid-cols-1 gap-6 mt-8">
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4"> &#128214; Educação: </h3>
-                        <ul className="list-disc list-inside text-gray-300 space-y-2">
-
-                            <li>
-                                <strong>Análise e Desenvolvimento de Sistemas </strong>  - IFSP Campus Votuporanga - <em>2018-2021</em>
-                            </li>
-
-                        </ul>
+                    {/* Educação */}
+                    <div className="grid grid-cols-2 md:grid-cols-1 gap-6 mt-8">
+                        <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">
+                                {" "}
+                                &#128214; Educação:{" "}
+                            </h3>
+                            <ul className="list-disc list-inside text-gray-300 space-y-2">
+                                <li>
+                                    <strong>
+                                        Análise e Desenvolvimento de
+                                        Sistemas{" "}
+                                    </strong>{" "}
+                                    - IFSP Campus Votuporanga -{" "}
+                                    <em>2018-2021</em>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-
-            </div>
-        </RevealOnScroll>
-    </section>
-}
+            </RevealOnScroll>
+        </section>
+    );
+};
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -8,7 +8,7 @@ export const Projects = () => {
             bg-clip-text text-transparent text-center">Projetos</h2>
 
                 {/* Projetos */}
-                <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
                         <h3 className="text-xl font-bold mb-4">NotesApp</h3>
                         <p className="text-gray-400 mb-4">Um aplicativo web para guardar notas</p>
@@ -23,7 +23,45 @@ export const Projects = () => {
                         {/* links */}
                         <div className="flex justify-between items-center">
                             <a href="https://github.com/martinaNasc04/notesApp"
-                                className="font-semibold text-blue-400 hover:text-purple-400 transition-colors">Ver projeto</a>
+                                className="font-semibold text-blue-400 hover:text-purple-400 transition-colors">Ver projeto no GitHub</a>
+                        </div>
+                    </div>
+                    <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                        <h3 className="text-xl font-bold mb-4">FindPet</h3>
+                        <p className="text-gray-400 mb-4"> Uma plataforma dedicada a unir animais perdidos e aqueles que buscam um lar </p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["Next.js", "React", "TailwindCSS", "Drizzle", "Typescript", "Clerk", "Cloudinary"].map((tech, key) => (
+                                <span key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">{tech}</span>
+                            ))}
+                        </div>
+
+                        {/* links */}
+                        <div className="flex justify-between items-center">
+                            <a href="https://github.com/martinaNasc04/find-pet"
+                                className="font-semibold text-blue-400 hover:text-purple-400 transition-colors">Ver projeto no GitHub</a>
+                            <a href="https://find-pet-dusky.vercel.app/"
+                                className="font-semibold text-blue-400 hover:text-purple-400 transition-colors">Acesse o site</a>
+                        </div>
+                    </div>
+                    <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                        <h3 className="text-xl font-bold mb-4">TakeNotes</h3>
+                        <p className="text-gray-400 mb-4">Aplicação web projetada para a criação e gerenciamento de notas de forma eficiente e intuitiva </p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["Next.js", "Prisma", "Tailwind", "Typescript", "Clerk"].map((tech, key) => (
+                                <span key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">{tech}</span>
+                            ))}
+                        </div>
+
+                        {/* links */}
+                        <div className="flex justify-between items-center">
+                            <a href="https://github.com/martinaNasc04/take-notes"
+                                className="font-semibold text-blue-400 hover:text-purple-400 transition-colors">Ver projeto no GitHub</a>
+                            <a href="https://take-notes-sable.vercel.app/"
+                                className="font-semibold text-blue-400 hover:text-purple-400 transition-colors">Acesse o site</a>
                         </div>
                     </div>
 
